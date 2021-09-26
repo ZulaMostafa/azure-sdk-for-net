3011d5a69def5328acd96503d32b3936cf2170fb
Salah Mostafa <v-samostafa@microsoft.com>
2021-09-26 18:19:47 +0200

    Added tests

--- sdk/cognitivelanguage/Azure.AI.Language.Conversations/tests/ConversationAnalysisClientLiveTests.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations/tests/ConversationAnalysisClientLiveTests.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Threading.Tasks;
+using Azure.AI.Language.Conversations.Models;
+using NUnit.Framework;
+
 namespace Azure.AI.Language.Conversations.Tests
 {
     public class ConversationAnalysisClientLiveTests : ConversationAnalysisTestBase<ConversationAnalysisClient>
@@ -8,6 +12,41 @@ namespace Azure.AI.Language.Conversations.Tests
         public ConversationAnalysisClientLiveTests(bool isAsync, ConversationAnalysisClientOptions.ServiceVersion serviceVersion)
             : base(isAsync, serviceVersion, null /* RecordedTestMode.Record /* to record */)
         {
+        }
+        private static string EnglishText = "We'll have 2 plates of seared salmon nigiri.";
+        private static string SpanishText = "Tendremos 2 platos de nigiri de salmón braseado.";
+
+        [Test]
+        public async Task AnalyzeConversation()
+        {
+            ConversationAnalysisClient client = Client;
+
+            AnalyzeConversationOptions options = new AnalyzeConversationOptions(
+               TestEnvironment.ProjectName,
+               TestEnvironment.DeploymentName,
+               EnglishText);
+
+            Response<AnalyzeConversationResult> response = await client.AnalyzeConversationAsync(options);
+
+            Assert.That(response.Value.Prediction.TopIntent, Is.EqualTo("Order"));
+        }
+
+        [Test]
+        public async Task AnalyzeConversationWithLanguage()
+        {
+            ConversationAnalysisClient client = Client;
+
+            AnalyzeConversationOptions options = new AnalyzeConversationOptions(
+               TestEnvironment.ProjectName,
+               TestEnvironment.DeploymentName,
+               SpanishText)
+            {
+                Language = "es"
+            };
+
+            Response<AnalyzeConversationResult> response = await client.AnalyzeConversationAsync(options);
+
+            Assert.That(response.Value.Prediction.TopIntent, Is.EqualTo("Order"));
         }
     }
 }
